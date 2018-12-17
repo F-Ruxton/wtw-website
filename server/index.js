04877f367ec1e1eabaf0de08eea689669820a9a1
Freddie Ruxton <freddie.ruxton@procensus.com>
2018-12-17 12:36:40 +0000

    huihiu

--- server/index.js
+++ server/index.js
@@ -31,14 +31,14 @@ if (!isDev && cluster.isMaster) {
   middleware.install(app);
   apis.register(app);
 
+  // Answer API requests.
+  app.get('/api', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
+  });
+
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../ui/build')));
-
-  // // Answer API requests.
-  // app.get('/api', function (req, res) {
-  //   res.set('Content-Type', 'application/json');
-  //   res.send('{"message":"Hello from the custom server!"}');
-  // });
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
